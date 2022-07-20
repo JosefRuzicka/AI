776522efba8eebcce932a4b7b523c1b0a5b592ca
Josef Ruzicka <jose.f.2000@hotmail.com>
2022-07-19 20:08:59 -0600

    Removed Cmake and Makefile files

--- NeuralNetwork/cmake-build-release/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/juanr/Desktop/NeuralNetwork-IA
-# Build directory: C:/Users/juanr/Desktop/NeuralNetwork-IA/cmake-build-release
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
